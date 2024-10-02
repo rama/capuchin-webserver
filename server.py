564291510476f0e23f73e5eb46643dee29f29ae2
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2024-10-01 21:36:49 -0400

    Serve index.html from public and nested folders

--- server.py
+++ server.py
@@ -20,15 +20,12 @@ def main(port=25600):
         start_line = request.split('\r\n')[0].split(' ')
         print(start_line)
         if start_line[0] == "GET":
-            #path = start_line[1].replace('/', '.', 1) 
-            #print(path)
-            path = "./index.html"
+            path = "./public/" + start_line[1] + "/index.html"
             with open(path, 'r') as f:
                 content = f.read()
-                print(len(content)) #is len in bytes???
         headers = parse_request_and_get_headers(request)
         print(headers)
-        response = construct_response()
+        response = construct_response(content)
         new_socket.sendall(response.encode("ISO-8859-1"))
         new_socket.close()
 
@@ -40,8 +37,8 @@ def parse_request_and_get_headers(request):
         headers[match[0]] = match[1]
     return headers
 
-def construct_response():
-    return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 10\r\nConnection: close\r\n\r\nHi, there!\r\n\r\n";
+def construct_response(content):
+    return f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(content)}\r\nConnection: close\r\n\r\n{content}\r\n\r\n";
 
 try:
     main()

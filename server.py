b7c6d21a79bf03bc0f7a1c2d91ae38967bc68e38
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2024-10-04 15:03:59 -0400

    Refactored with help from Justin Schulz <jtschulz@users.noreply.github.com>

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import re
 import socket
 import os
 
-SERVER_ROOT = os.path.abspath("./public")
 STATUSES = {
     "200": "OK",
     "404": "Not Found",
@@ -17,72 +16,107 @@ MIME_TYPES = {
     ".gif": "image/gif",
 }
 
-def main(port=25600):
-    s = socket.socket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    address = ('', port)
-    s.bind(address)
-    s.listen()
-    # Needs to run forever!!
-    while (True):
-        new_connection = s.accept()
+# x create a Server class
+# x it can hold above constants
+# x add a run method
+# x not needed yet - add a setup method to choose port etc?
+# x not needed yet - should i track state?
+# maybe move to create_socket etc
+# x method for now - parse request in separate class or method?
+# TODO construct response - should response be a dataclass?
+# TODO add pylint and mypy type annotations?
+
+
+class Server:
+    def __init__(self, port=25600, server_root="./public"):
+        self.PORT = port
+        self.self.SERVER_ROOT = os.path.abspath(server_root)
+        self.socket = socket.socket()
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        address = ("", self.PORT)
+        self.socket.bind(address)
+
+    def run(self):
+        self.socket.listen()
+        print("Server running...")
+
+        while True:
+            self._handle_request()
+
+    def _handle_request(self):
+        new_connection = self.socket.accept()
         new_socket = new_connection[0]
         req = new_socket.recv(1024)
         request = req.decode("ISO-8859-1")
-        while ("\r\n\r\n" not in req.decode()):
+        while "\r\n\r\n" not in req.decode():
             req = new_socket.recv(1024)
             request += req.decode("ISO-8859-1")
         print(request)
-        start_line = request.split('\r\n')[0].split(' ')
+        start_line = request.split("\r\n")[0].split(" ")
         print(start_line)
         if start_line[0] == "GET":
-            file_path = os.path.abspath(os.path.sep.join([SERVER_ROOT, start_line[1]]))
-            if file_path.startswith(SERVER_ROOT):
-                file_path = add_index_to_filepath(file_path)
-                extension = os.path.splitext(file_path)[-1]
-                content_type = MIME_TYPES[extension]
-                try:
-                    with open(file_path, 'r') as f:
-                        content = f.read()
-                        response = construct_response("200", content, content_type)
-                except:
-                    # send 404
-                    print("404 Not Found")
-                    content = "not found"
-                    response = construct_response("404", content)
-            else:
-                # send 403
-                print("403 Forbidden")
-                content = "forbidden"
-                response = construct_response("403", content)
-        
-        headers = parse_request_and_get_headers(request)
-        # print(content)
+            response = self._handle_get(start_line[1])
+
+        # headers = parse_request_and_get_headers(request)
         new_socket.sendall(response.encode("ISO-8859-1"))
         new_socket.close()
 
+    def _handle_get(self, target):
+        file_path = os.path.abspath(os.path.sep.join([self.SERVER_ROOT, target]))
+        # Make sure requested file is inside server_root
+        if not file_path.startswith(self.SERVER_ROOT):
+            print("403 Forbidden")
+            content = "forbidden"
+            return construct_response("403", content)
+
+        file_path = add_index_to_filepath(file_path)
+        extension = os.path.splitext(file_path)[-1]
+        content_type = MIME_TYPES[extension]
+        try:
+            with open(file_path, "r") as f:
+                content = f.read()
+                return construct_response("200", content, content_type)
+        except:
+            print("404 Not Found")
+            content = "not found"
+            return construct_response("404", content)
+
+    def stop(self):
+        pass
+
+
 def parse_request_and_get_headers(request):
     headers = {}
-    pattern = re.compile(r'(.+?): (.+?)\r\n')
+    pattern = re.compile(r"(.+?): (.+?)\r\n")
     matches = re.findall(pattern, request)
     for match in matches:
         headers[match[0]] = match[1]
     return headers
 
+
 def add_index_to_filepath(path):
     # check if the file_path ends with .html or a trailing slash?
-    if not path.endswith('.html'):
-        if path.endswith('/'): 
-            path += 'index.html'
+    if not path.endswith(".html"):
+        if path.endswith("/"):
+            path += "index.html"
         else:
-            path += '/index.html'
+            path += "/index.html"
     return path
 
+
 def construct_response(status_code, content, content_type="text/plain"):
-    return (f"HTTP/1.1 {status_code} {STATUSES[status_code]}\r\n" + 
-            f"Content-Type: {content_type}\r\n" + 
-            f"Content-Length: {len(content)}\r\n" + 
-            f"Connection: close\r\n\r\n{content}\r\n\r\n");
+    return (
+        f"HTTP/1.1 {status_code} {STATUSES[status_code]}\r\n"
+        + f"Content-Type: {content_type}\r\n"
+        + f"Content-Length: {len(content)}\r\n"
+        + f"Connection: close\r\n\r\n{content}\r\n\r\n"
+    )
+
+
+def main():
+    server = Server()
+    server.run()
+
 
 try:
     main()
